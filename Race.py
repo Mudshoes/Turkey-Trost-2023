8afd516928e0e3d8401137b85e15968a0b55abfc
Mudshoes <kg082108@wgcloud.org>
2023-11-16 09:49:09 -0600

    Coded and tested code to generate turkeys for race. Added bases for the race code itself.

---
+++ Race.py
@@ -0,0 +1,53 @@
+from random import randint
+from turtle import speed
+'''
+class turkey:
+    def __init__(self,spd,agi,int,chance):
+        self.spd = spd
+        self.agi = agi
+        self.int = int
+        self.chance = chance
+
+    def randStats(self):
+        self.spd = randint(1,10)
+        self.agi = randint(1,10)
+        self.int = randint(1,10)
+
+
+turkey1 = turkey(0,0,0,0)
+turkey1.randStats()
+'''
+
+
+
+## Generates all of the turkeys for the race with randomized stats
+## Might add a system that calculates their odds of winning later
+## Might also add randomized names.
+def turkeyGen(turkeyNum):
+    turkeyDict = {
+
+    }
+
+    for i in range(turkeyNum):
+        tempList = []
+
+        ## Generates 3 values for each turkey, speed, agility, and intelligence
+        ## Speed is obvious, agility determines chance to dodge, and
+        ## intelligence determines chance to beat obstacles.
+        for j in range(3):
+            tempList.append(randint(1,10))
+
+        turkeyDict.update({str("Turkey " + str(i + 1)): list(tempList)})
+        tempList.clear()
+    
+    print(turkeyDict.items())
+    return turkeyDict
+
+def turkeyRace(turkeyDict):
+    pass
+
+while True:
+    turkeyNum = int(input("How many turkeys will participate in the race? "))
+    turkeyDict = turkeyGen(turkeyNum)
+    turkeyRace(turkeyDict)
+
